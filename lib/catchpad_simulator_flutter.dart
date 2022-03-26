13b8dfebdff5b0958703dfd0c306e7c09fe6378b
Adnan <adnanjpg@github.com>
2022-03-26 10:54:36 +0300

    import device model from main catchpad app

--- lib/catchpad_simulator_flutter.dart
+++ lib/catchpad_simulator_flutter.dart
@@ -1,7 +1,9 @@
 export 'src/catchpad_simulator.dart';
 
 export 'src/enums/enviroment_type.dart';
+
 export 'src/models/enviroment_model.dart';
+export 'src/models/device_model.dart';
 
 export 'src/provs/enviroment_prov.dart';
 export 'src/provs/ble_prov.dart';
---
+++ lib/src/models/device_model.dart
@@ -0,0 +1,38 @@
+import 'package:flutter_reactive_ble/flutter_reactive_ble.dart';
+
+typedef DeviceModel = DiscoveredDevice;
+typedef DevList = List<DeviceModel>;
+
+extension CatchpadConnection on ConnectionStateUpdate {
+  bool get isConnected => connectionState == DeviceConnectionState.connected;
+}
+
+extension CpDiscoveredDevice on DiscoveredDevice {
+  bool get isCPDevice => name.toLowerCase().contains('catchpad');
+
+  int? get deviceNumber {
+    final d = deviceNameId;
+    if (d == null) return null;
+    return int.tryParse(d);
+  }
+
+  String? get deviceNameId {
+    final sp = name.split(' ');
+
+    if (sp.length == 2) {
+      return sp[0];
+    }
+
+    return null;
+  }
+
+  static Map<String, String?> devIdNameIdMap(Iterable<DeviceModel> devs) {
+    return devs.fold(
+      {},
+      (Map<String, String?> map, DeviceModel dev) {
+        map[dev.id] = dev.deviceNameId;
+        return map;
+      },
+    );
+  }
+}
